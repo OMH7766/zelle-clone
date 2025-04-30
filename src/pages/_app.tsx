09c7268785e4bd6448222bec8e710d5863c50505
OMH7766 <othmanalhasan76@gmail.com>
2025-04-30 15:56:54 +0300

    Fix AppProps typing in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
-import '../styles/globals.css';
-import { SessionProvider } from 'next-auth/react';
-
-export default function App({ Component, pageProps }) {
-  return (
-    <SessionProvider session={pageProps.session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  );
-}
+-import '../styles/globals.css';
+-import { SessionProvider } from 'next-auth/react';
+-
+-export default function App({ Component, pageProps }) {
+-  return (
+-    <SessionProvider session={pageProps.session}>
+-      <Component {...pageProps} />
+-    </SessionProvider>
+-  );
+-}
++import '../styles/globals.css';
++import { SessionProvider } from 'next-auth/react';
++import type { AppProps } from 'next/app';
++
++export default function App({ Component, pageProps }: AppProps) {
++  return (
++    <SessionProvider session={pageProps.session}>
++      <Component {...pageProps} />
++    </SessionProvider>
++  );
++}
